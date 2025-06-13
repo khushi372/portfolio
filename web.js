ed42af095617f1d810ed6e7fc9de2f3edd418919
Khushi Patel <khushipatel65801@gmail.com>
2025-06-13 12:00:21 +0530

    Add responsive projects

--- web.js
+++ web.js
@@ -2,12 +2,13 @@ let burger = document.querySelector("#bars");
 let nav2 = document.querySelector(".nav2");
 let contact = document.querySelector(".Contact");
 let exit = document.querySelector("#exit");
+let navLink = document.querySelectorAll(".nav2 nav a");
 
-burger.addEventListener('click',function(){
+burger.addEventListener('click', function () {
   nav2.classList.add('show');
 });
 
-exit.addEventListener('click',function(){
+exit.addEventListener('click', function () {
   nav2.classList.remove('show');
 });
 // console.log(burger);
@@ -18,18 +19,32 @@ let button = document.querySelector(".scroll-btn");
 
 console.log(button);
 
-window.addEventListener('scroll',function(){
+window.addEventListener('scroll', function () {
   console.log(window.scrollY);
-  if(this.window.scrollY > 200){
-      button.classList.remove("scroll-btn-visibility");
-  }else{
-      button.classList.add("scroll-btn-visibility");
+  if (this.window.scrollY > 200) {
+    button.classList.remove("scroll-btn-visibility");
+  } else {
+    button.classList.add("scroll-btn-visibility");
   }
 });
 
-button.addEventListener('click',function(){
+button.addEventListener('click', function () {
   document.documentElement.scrollTo({
-      top:0,
-      behavior:"smooth",
+    top: 0,
+    behavior: "smooth",
   });
 });
+
+console.dir(navLink);
+
+navLink.forEach(element => {
+  element.addEventListener('click', function () {
+    nav2.classList.remove('show');
+  });
+});
+
+// navLink.addEventListener('click', function () {
+//   nav2.classList.remove('show');
+// });
+
+// Object.addEventListener
